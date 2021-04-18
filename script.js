3a8a10cb2e587a65f99420210687a10c521f54cf
Sofya Kiseleva-Komolkina <skomolkina@gmail.com>
2021-04-18 16:45:37 +0300

    buttons & fonts

---
+++ script.js
@@ -0,0 +1,12 @@
+let buttonEditOpen = document.querySelector('.profile__btn_edit');
+let buttonEditClose = document.querySelector('.popup__cross');
+let formEditName = document.querySelector('.popup__input-name');
+let formEditAbout = document.querySelector('.popup__input-about');
+
+
+function profileEdit () {
+    buttonEditOpen.addEventListener('click', profileEdit);
+    
+} 
+
+buttonEditOpen.addEventListener('click', )
