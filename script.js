68416e0201bce3cfdd5be1f457861e9ec87a5e15
Sofya Kiseleva-Komolkina <skomolkina@gmail.com>
2021-04-19 16:48:23 +0300

    added popup edit js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const buttonEditOpen = document.querySelector('.profile__btn_edit');
-const buttonEditClose = document.querySelector('.popup__cross');
-const popup = document.querySelector('.popup__closed');
-const formElement = document.querySelector('.popup__input')
-const name = document.querySelector('.profile__name');
-const job = document.querySelector('.profile__about');
-const nameInput = document.querySelector('.popup__input-name')
-const jobInput = document.querySelector('.popup__input-about')
+let buttonEditOpen = document.querySelector('.profile__btn_edit');
+let buttonEditClose = document.querySelector('.popup__cross');
+let popup = document.querySelector('.popup');
+let formElement = document.querySelector('.popup__container')
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__about');
+let nameInput = document.querySelector('.popup__input-name')
+let jobInput = document.querySelector('.popup__input-about')
 
 function openEdit () {
     popup.classList.remove('popup__closed');
@@ -18,50 +18,9 @@ function closeEdit () {
 buttonEditClose.addEventListener('click', closeEdit);
 
 
-// Находим форму в DOM
-
-// Воспользуйтесь методом querySelector()
-
-// Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
-function formSubmitHandler (evt) {
-	evt.preventDefault();
-    // let inputName = nameInput.value;
-    // let inputjob = jobInput.value;
-
-	// Выберите элементы, куда должны быть вставлены значения полей
-
-	// Вставьте новые значения с помощью textContent
-    // name.innerHTML = nameInput.value;
-    // job.innerHTML = jobInput.value;
-
-    name.textContent = nameInput.va;
-    
-    formElement.addEventListener('submit', formSubmitHandler);
-
-    popup.classList.remove('.popup__closed');
-}
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-
-
-
-
-
-
-
-/*
-const formElement = document.querySelector('.popup__container');
-const profileName = document.querySelector('.profile__name');
-const profileAbout = document.querySelector('.profile__about');
-
-function formSubmitHandler (evt) {
+formElement.addEventListener('submit', function (evt) {
     evt.preventDefault();
-    const inputName = document.querySelector('.popup__input-name');
-    const inputAbout = document.querySelector('.popup__input-about');
-    inputName.textContent = profileName.value;
-    inputAbout.textContent = profileAbout.value;
-}
-    
-formElement.addEventListener('submit', formSubmitHandler);*/
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
+    popup.classList.add('popup__closed');
+});
