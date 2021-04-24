56fbacdea93a7f65fa829dd814ab6299d425c154
Sofya Kiseleva-Komolkina <skomolkina@gmail.com>
2021-04-24 15:19:08 +0300

    fix for third review

--- scripts/script.js
+++ scripts/script.js
@@ -4,10 +4,14 @@ let popup = document.querySelector('.popup'); // окно редактирова
 let formElement = document.querySelector('.popup__container'); // форма редактирования
 let profileName = document.querySelector('.profile__name'); // отображение имени на гл.стр.
 let profileJob = document.querySelector('.profile__about'); // отображение деятельности на гл.стр.
+let nameInput = document.querySelector('.popup__form_type_name'); // ввод имени
+let jobInput = document.querySelector('.popup__form_type_about'); // ввод деятельности
 
 
 function openEdit () {
     popup.classList.add('popup_opened');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 } 
 
 function closeEdit () {
@@ -15,11 +19,8 @@ function closeEdit () {
 }
 
 function formSubmitHandler (evt) {
-	evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-	// Находим поля формы в DOM
-	let nameInput = document.querySelector('.popup__form_type_name'); // ввод имени
-	let jobInput = document.querySelector('.popup__form_type_about'); // ввод деятельности
-
+	evt.preventDefault(); 
+    
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
 
